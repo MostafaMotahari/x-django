f46720e4fed25ecccf9a05ed149f3c826097fdc6
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-05-08 19:14:08 +0330

    create needed models for profile.

--- x_profile/models.py
+++ x_profile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class XrayUser(AbstractUser):
+    telegram_user_id = models.CharField(max_length=18)
+    current_service = models.ForeignKey('XrayServer', models.SET_NULL, 'server_users', null=True, blank=True)
+
+
+class XrayServer(models.Model):
+    country = models.CharField(max_length=128)
+    capacity = models.PositiveIntegerField(default=0)
