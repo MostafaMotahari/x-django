e25dd685a019f4ef9f3fdfbd706660bbb2f1437c
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-05-09 19:46:34 +0330

    minor changes.

--- x_bot/filters/custom_filters.py
+++ x_bot/filters/custom_filters.py
@@ -1,4 +1,4 @@
 from pyrogram import filters
 from x_bot.plugins.start import start
 
-force_join_filter = filters.create(lambda _, __: start(_, __, False))
+force_join_filter = filters.create(lambda _, __, msg: start(__, msg, False))
--- x_bot/plugins/free_service.py
+++ x_bot/plugins/free_service.py
@@ -7,7 +7,7 @@ from x_profile import schema
 from x_bot.filters.custom_filters import force_join_filter
 
 
-@Client.on_callback_query(filters.regex("^free_(.*)$") & force_join_filter)
+@Client.on_callback_query(filters.regex("^free_(.*)$"))
 def free_v2ray(client, callback_query):
     remark = str(callback_query.from_user.id) + callback_query.data.split("_")[-1]
     stream_setting = schema.STREAM_SETTINGS
