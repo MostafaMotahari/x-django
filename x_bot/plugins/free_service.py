c0703002eb29ef9c3107015545bd33f25e136ba8
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-05-09 20:22:14 +0330

    bug fix.

--- x_bot/plugins/free_service.py
+++ x_bot/plugins/free_service.py
@@ -13,18 +13,18 @@ def free_v2ray(client, callback_query):
     stream_setting = schema.STREAM_SETTINGS
     sniffing_setting = schema.SNIFFING_SETTINGS
     inbound_setting = schema.INBOUND_SETTINGS
-    inbound_setting['id'] = "12"
-    inbound_setting['tagId'] = remark
+    inbound_setting['client']['id'] = "12"
+    inbound_setting['client']['tagId'] = remark
 
     inbound = profile_models.Inbound.objects.create(
         user_id = callback_query.from_user.id,
         remark = remark,
         port=callback_query.from_user.id % 10000,
         protocol='vmess',
-        settings=json.dumps(inbound_setting),
-        stream_settings=json.dumps(stream_setting),
-        tag='inbound' + str(callback_query.from_user.id),
-        sniffing=json.dumps(sniffing_setting)
+        settings=inbound_setting,
+        stream_settings=stream_setting,
+        tag='inbound-' + str(callback_query.from_user.id),
+        sniffing=sniffing_setting
     )
 
     client.send_message(callback_query.chat.id, 'Created')
