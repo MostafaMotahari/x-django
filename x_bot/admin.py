8ecfd62765325c1dba53571dbe5061d9eac54a1a
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-05-18 21:35:08 +0330

    models admin added.

--- x_bot/admin.py
+++ x_bot/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.XrayUser)
+class XrayUserAdmin(admin.ModelAdmin):
+    list_display = ('telegram_user_id', )
+
+
+@admin.register(models.XrayServer)
+class XrayServerAdmin(admin.ModelAdmin):
+    list_display = ('country', 'is_active', 'capacity', 'domain')
+
+
+@admin.register(models.XrayService)
+class XrayServiceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'server', 'price')
+
+
+@admin.register(models.XrayPort)
+class XrayPortAdmin(admin.ModelAdmin):
+    list_display = ('server', 'port_number', 'user')
